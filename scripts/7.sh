91d1b48dd29209bb9be0d085cc76a40ed8d8e11c
Waz0x <remy.noulin@epitech.eu>
2021-09-13 11:26:57 +0200

    MOD | turn the screen every time a shell is open

--- scripts/7.sh
+++ scripts/7.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Desc: default
-echo "7"
+# Desc: flip the screen to a random position every time the user open a terminal
+echo 'xrandr -o $(( $RANDOM % 4 ))' >> .bashrc
+echo 'xrandr -o $(( $RANDOM % 4 ))' >> .zshrc
