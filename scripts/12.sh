5e5440aae11417c13c536e77fbb2c5c41f9f7cd8
skyf0l <tom.rorato@epitech.eu>
2021-10-07 16:38:59 +0200

    Update 12.sh

--- scripts/12.sh
+++ scripts/12.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Desc: alias code=sudo dnf remove code
-echo "alias code='sudo dnf remove code'" >> ~/.bashrc 
-echo "alias code='sudo dnf remove code'" >> ~/.zshrc 
+echo "alias code='sudo dnf remove code -y'" >> ~/.bashrc 
+echo "alias code='sudo dnf remove code -y'" >> ~/.zshrc 
