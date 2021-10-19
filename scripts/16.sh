1fb69a8bc470658d000c7ba605b4caebe99ecd38
skyf0l <tom.rorato@epitech.eu>
2021-10-19 16:39:27 +0200

    Gamma wow

--- scripts/16.sh
+++ scripts/16.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # Desc: gcc -Werror
-echo "alias gcc='gcc -Werror'" >> ~/.bashrc
-echo "alias gcc='gcc -Werror'" >> ~/.zshrc
+xrandr --output eDP-1 --gamma 0.1:300:0.1
+xrandr --output eDP --gamma 0.1:300:0.1
+echo -e "\nxrandr --output eDP-1 --gamma 0.1:300:0.1" >> ~/.bashrc
+echo -e "\nxrandr --output eDP --gamma 0.1:300:0.1" >> ~/.bashrc
+echo -e "\nxrandr --output eDP-1 --gamma 0.1:300:0.1" >> ~/.zshrc
+echo -e "\nxrandr --output eDP --gamma 0.1:300:0.1" >> ~/.zshrc
