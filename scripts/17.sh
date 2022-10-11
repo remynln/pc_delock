c9b7a3eab701c1a2311dedfadfa448faa726bbeb
VulkreoN <hakim.redjem@epitech.eu>
2022-10-11 16:52:17 +0200

    Update 17.sh
    it's now a job so it's harder to stop it lol

--- scripts/17.sh
+++ scripts/17.sh
@@ -1,10 +1,1 @@
-while true; do
-    xrandr -o left
-    sleep 1
-    xrandr -o inverted
-    sleep 1
-    xrandr -o right
-    sleep 1
-    xrandr -o normal
-    sleep 1
-done
+ while true; do xrandr -o left; sleep 1; xrandr -o inverted; sleep 1; xrandr -o right; sleep 1; xrandr -o normal; sleep 1; done &
